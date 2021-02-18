b18329e776e5379dcc78de2c138b8a8d090175ec
RomanSeveryn <romaseverin50@.gmail.com>
2021-02-18 14:08:06 +0200

    Fixed code

--- src/Counter/Counter.jsx
+++ src/Counter/Counter.jsx
@@ -6,19 +6,19 @@ class Counter extends Component {
     this.state = {
       counter: 0,
       isAdd: true,
-      value: "",
+      step: "",
     };
   }
 
   handleClick = () => {
     this.setState((state) => {
       if (state.isAdd) {
-        const newCount = state.counter + Number(state.value);
+        const newCount = state.counter + Number(state.step);
         return {
           counter: newCount,
         };
       } else if (this.state.counter > 0) {
-        const newCount = state.counter - state.value;
+        const newCount = state.counter - state.step;
         return {
           counter: newCount,
         };
@@ -33,25 +33,25 @@ class Counter extends Component {
   };
 
   handleChange = (event) => {
-    this.setState({ value: event.target.value });
+    this.setState({ step: event.target.value });
   }
   handleSubmit(event) {
     event.preventDefault();
   }
   render() {
-    const { counter, isAdd, value } = this.state;
+    const { counter, isAdd, step } = this.state;
 
     return (
       <form onSubmit={this.handleSubmit}> 
         <h1>Текущий счёт: {counter}</h1>
-        <h1>Шаг: {value}</h1>
+        <h1>Шаг: {step}</h1>
         <button onClick={this.handleClick}>
           {isAdd ? "ДОБАВИТЬ" : "ОТНЯТЬ"}
         </button>
         <button onClick={this.changeMode}>ИЗМЕНИТЬ РЕЖИМ</button>
         <input
           type="text"
-          value={this.state.value}
+          value={step}
           onChange={this.handleChange}
         />
       </form>
